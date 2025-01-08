0b6b8d4920aced52d6e2be8c792707330d833eb7
Ihury Prado <ihurycprado@gmail.com>
2025-01-08 19:00:26 -0300

    atualizações do dia 07/01/2025

--- contact/admin.py
+++ contact/admin.py
@@ -4,13 +4,13 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'email','phone'
+    list_display = 'id', 'first_name', 'last_name', 'email','phone', 'show', 
     ordering = 'id', 
     # list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 20
     list_max_show_all = 100
-    list_editable = 'phone', 
+    list_editable = 'phone', 'show', 
     list_display_links = 'id', 'first_name',
 
 @admin.register(models.Category)
