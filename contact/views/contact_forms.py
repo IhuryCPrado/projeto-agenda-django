200141b5e29b9999a3b32763c9dbbce20a7b633f
Ihury Prado <ihurycprado@gmail.com>
2025-01-09 18:47:45 -0300

    atualizações do dia 08/01/2025

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator
-from django.db.models import Q
 
-from contact.models import Contact
+from contact.forms import ContactForm
 
 def create(request):
-    context = {
-        
-    }
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
 
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+    
+    context = {
+            'form': ContactForm()
+        }
+    
     return render(
-        request,
-        'contact/create.html',
-        context
-    )
+            request,
+            'contact/create.html',
+            context
+        )
